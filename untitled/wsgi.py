b6d2e8af8ab05e1c10086cafb0321810ddb2553a
Angle-Zz <130260417+Angle-Zz@users.noreply.github.com>
2023-05-10 15:02:07 +0800

    Update wsgi.py

--- untitled/wsgi.py
+++ untitled/wsgi.py
@@ -4,6 +4,7 @@
 它将WSGI可调用对象公开为'application'的模块变量
 """
 
+"""
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -11,3 +12,16 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'untitled.settings')
 
 application = get_wsgi_application()
+"""
+import os
+import sys
+
+path = '/home/Angle-Zz/HTML'  # use your own username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'untitled.settings'
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())

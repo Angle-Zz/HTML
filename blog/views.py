06cc36f97725b42dd4cca33b7cfce2e3ffd443b9
Want her <2946410997@qq.com>
2023-05-11 17:21:16 +0800

    Commit message

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/Home.html', {})
+
+def redirect_to_login(request):
+    return HttpResponseRedirect('/login/')
+
+def redirect_to_register(request):
+    return HttpResponseRedirect('/register')
+
+def redirect_to_image_preview(request):
+    return HttpResponseRedirect('image-preview')
 
